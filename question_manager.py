0002b7c09c9060ffbbbc7e31008ef61bd5044924
Avatudarbuus <alicja.ainsoo@gmail.com>
2025-04-19 12:26:39 +0300

    maci omanik, võiks mõlemal töödata

--- question_manager.py
+++ question_manager.py
@@ -7,14 +7,19 @@ Requirements:
 -output follows this format: Q: Question; A) Answer1; B) Answer2; C) Answer3; D) Answer4; Answer:
 """
 
+   if platform.system() == 'Windows':
+        ollama_path = r'C:\Users\aston\AppData\Local\Programs\Ollama\ollama.exe'
+    else:
+        ollama_path = 'ollama'
+
     result = subprocess.run(
-        [r"C:\Users\aston\AppData\Local\Programs\Ollama\ollama.exe", "run", "mistral"],
-        input=prompt,
+        [ollama_path, "run", "mistral"],
+        input = prompt,
         text=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         encoding='UTF-8'
-    )
+    ) # kontrollib mis platform on ja siis saame mõlemad oma arvutis tööle
     print(type(result.stdout))
     print(result.stdout)
     return result.stdout
